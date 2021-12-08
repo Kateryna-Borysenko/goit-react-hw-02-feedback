cc2f1cd279b1161dcdce01664295541f16e88282
Kateryna-Borysenko <k.borysenko.kyiv@gmail.com>
2021-12-08 23:34:02 +0200

    Added Section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import image from 'images/feedback.jpeg';
 import Statistics from 'components/Statistics/Statistics';
 import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
+import Section from 'components/Section/Section';
 
 export class App extends Component {
   state = {
@@ -25,7 +26,7 @@ export class App extends Component {
     const total = this.countTotalFeedback();
     const good = this.state.good; //
     //Math.ceil()-> округление до большего целого
-    //eсли total -> есть то считаем, если ничего не выбрано значение 0
+    //eсли total -> есть то считаем, если ничего не выбрано значение 0 без проверки NaN
     return total ? Math.ceil((good / total) * 100) : 0;
   };
 
@@ -33,25 +34,30 @@ export class App extends Component {
     const { good, neutral, bad } = this.state;
     return (
       <div>
-        {/* Section start */}
         <h1 className="title">Please leave your feedback</h1>
         <img src={image} alt="Feedback" />
-        {/* Section end */}
 
         <FeedbackOptions
           categories={['good', 'neutral', 'bad']}
           onClickBtn={this.onClickBtn} //cсылка на ф-цию
         />
-        <h2 className="title">Statistics</h2>
 
-        {/* диструктуризация строка 19*/}
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={this.countTotalFeedback()} //результат вызова
-          percentage={this.countPositiveFeedbackPercentage()} //результат вызова
-        />
+        <Section title="Statistics">
+          {/* диструктуризация строка 19*/}
+
+          {/*  блок Statistics рендерился только после того, как был собран хотя бы один отзыв */}
+          {this.countTotalFeedback() ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()} //результат вызова
+              percentage={this.countPositiveFeedbackPercentage()} //результат вызова
+            />
+          ) : (
+            'There is no feedback'
+          )}
+        </Section>
       </div>
     );
   }
